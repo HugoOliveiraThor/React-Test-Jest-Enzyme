a8e10b47545a40e6cf9e91856fda6582fb52781b
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2019-01-14 19:58:03 -0200

    Start tests

--- __test__/counter.test.js
+++ __test__/counter.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import App from '../src/App'
+import Counter from '../src/counter'
 
 describe('Testing App Component', () => {
   it('should render correctly', () => {
     const wrapper = shallow(
-      <App />
+      <Counter count={5} />
     );
 
+    expect(wrapper).toMatchSnapshot();// This take a snapshot that will compare if the component has the same behavior as the first snapshot
+    wrapper.setProps({ count: 10});
     expect(wrapper).toMatchSnapshot();// This take a snapshot that will compare if the component has the same behavior as the first snapshot
   })
 })
